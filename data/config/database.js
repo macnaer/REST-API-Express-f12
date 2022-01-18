5f43e7da881987c7e058016a9b099c31c3321943
Robert-gut <barnabirobin1@gmail.com>
2022-01-18 19:08:19 +0200

    new branch

--- data/config/database.js
+++ data/config/database.js
@@ -1,9 +1,8 @@
 const Sequelize = require('sequelize');
 const Config = require("./settings");
 
-
 module.exports = new Sequelize(Config.DATABASE_NAME, Config.DATABASE_USER, Config.DATABASE_PASS, {
-    dialect: Config.DATABASE_DIALECT,
-    host: Config.DATABASE_URL,
-    port: Config.DATABASE_PORT
-  });
+  dialect: Config.DATABASE_DIALECT,
+  host: Config.DATABASE_URL,
+  port: Config.DATABASE_PORT
+});
