7492b5c2dacf768905abfea4f68a20408d8c4141
fierodev <andriybigyn31@gmail.com>
2022-01-20 19:16:30 +0200

    vommit

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,22 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Route, Routes } from "react-router";
-<<<<<<< HEAD
 import { BrowserRouter as Router} from "react-router-dom";
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
->>>>>>> 01354044ae604521d08ab54cdf662d6661713c87
 
 // Store
 import { Provider } from "react-redux";
 import { store } from "./store";
 
 // Componnents
-<<<<<<< HEAD
 import DashBoard from './Components/Containers/Layout/Dashboard';
 // import DashBoard from './Components/Dashboard/Dashboard';
 import Registration from './Components/Registration/Registration'
@@ -24,23 +20,16 @@ import LoginPage from './Components/LoginPage/LoginPage';
 import DefaultLayout from "./Components/Containers/Layout/defaultLayout";
 import Main from "./Components/Main/index";
 import Admin from "./Components/Admin/index"
-=======
-import LoginPage from "./Components/LoginPage/LoginPage";
-import DefaultLayout from "./Components/Containers/Layout/defaultLayout";
-import Main from "./Components/Main/index";
+import EditProfile from "./Components/EditProfile/EditProfile";
 
-import Admin from "./Components/Admin/index";
-import Registration from "./Components/Registration/Registration";
-
-import Admin from "./Components/Admin/index";
 import ProfilePage from "./Components/Profile/ProfilePage";
->>>>>>> 01354044ae604521d08ab54cdf662d6661713c87
 
 // Hoc ApiService
 import ApiService from "./Services/ApiService";
 import { ApiStoreServiceProvider } from "./Components/Api-service-context/Api-service-context";
 import Dashboard from "./Components/Containers/Layout/Dashboard";
 import UserList from "./Components/UserList/Userlist";
+// import EditProfile from "./Components/EditProfile/EditProfile";
 const apiService = new ApiService();
 
 const App = () => {
@@ -58,6 +47,8 @@ const App = () => {
               <Route path="/adminPanel/admin" exact element={<Admin />} />
 
               <Route path="/adminPanel/userList" element={<UserList />} />
+
+              <Route path="/adminPanel/editProfile" element={<EditProfile />} />
 
               <Route path="/adminPanel/userList" element={<UserList />} />
               <Route path="/adminPanel/profile" element={<ProfilePage />} />
