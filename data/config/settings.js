ced3334af6722266a45192f173e7c4e3aebfe8c4
RomanMelnyk909 <45914004+RomanMelnyk909@users.noreply.github.com>
2022-01-27 21:36:50 +0200

    fix

--- data/config/settings.js
+++ data/config/settings.js
@@ -10,7 +10,6 @@
 
 module.exports = Config = {
   // DATABASE_URL: '194.44.93.225',
-<<<<<<< HEAD
 
   // DATABASE_URL: '10.7.101.197',
 
@@ -26,15 +25,6 @@ module.exports = Config = {
 
   DATABASE_PASS: "RpYP>$K((2Y$:_6K",
 
-=======
-  // DATABASE_URL: '10.7.101.197',
-  DATABASE_URL: '52.168.23.170',
-  DATABASE_NAME: 'Node_f12',
-  // DATABASE_USER: "test",
-  DATABASE_USER: "sa",
-  // DATABASE_PASS: "1",
-  DATABASE_PASS: "RpYP>$K((2Y$:_6K",
->>>>>>> DEV
   DATABASE_PORT: 1433,
 
   DATABASE_DIALECT: "mssql",
