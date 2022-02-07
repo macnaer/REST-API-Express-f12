d44b1456f49c7d920fc3a753412480abcf68f82f
macnaer <trofimchuk.an@gmail.com>
2022-02-07 13:55:47 +0200

    Update settings.js

--- data/config/settings.js
+++ data/config/settings.js
@@ -1,6 +1,6 @@
 module.exports = Config = {
-  DATABASE_URL: "194.44.93.225",
-  // DATABASE_URL: '10.7.101.197',
+  //DATABASE_URL: "194.44.93.225",
+  DATABASE_URL: '10.7.101.197',
   // DATABASE_URL: '52.168.23.170',
   DATABASE_NAME: "Node_f12",
   DATABASE_USER: "test",
